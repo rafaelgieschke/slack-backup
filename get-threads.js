52d5e3a6f28fcef4997765395193eab1b79ca19a
Rafael Gieschke <rafael@gieschke.de>
2023-06-09 10:40:17 +0200

    feat(get-threads): use cookie instead of token

--- get-threads.js
+++ get-threads.js
@@ -1,27 +1,39 @@
 #!/bin/sh
 //bin/sh -c :; exec deno run -A "$0" "@"
 
-import token from "./token.json" assert { type: "json" };
-
 const $$ = (v) => (
   console.dir(v, { depth: Infinity, strAbbreviateSize: Infinity }), v
 );
 
 Object.defineProperty(Object.prototype, "$$", {
-  get() {
-    return $$(this.valueOf());
+  get(v = this.valueOf()) {
+    return console.dir(v, { depth: Infinity, strAbbreviateSize: Infinity }), v;
   },
 });
+
+///////////////////////////////////////////////////////////////////////////////
+
+const cwd = Deno.cwd();
 
 const stdout = async (cmd, ...args) =>
   new TextDecoder().decode(
     (await new Deno.Command(cmd, { args }).output()).stdout
   );
 
+const writeText = async (path, text) => {
+  await Deno.writeTextFile(path, text);
+};
+const readText = async (path) => {
+  await Deno.readTextFile(path);
+};
+
+///////////////////////////////////////////////////////////////////////////////
+
 class Client {
   base = "https://slack.com/api/";
-  constructor({ token, team_id }) {
+  constructor({ token, cookie, team_id }) {
     this.token = token;
+    this.cookie = cookie;
     this.team_id = team_id;
   }
   async api(method, args) {
@@ -29,6 +41,7 @@ class Client {
     url.search = String(
       new URLSearchParams({
         team_id: this.team_id,
+        limit: 1000,
         ...args,
       })
     );
@@ -36,6 +49,7 @@ class Client {
       await fetch(url, {
         headers: {
           authorization: `Bearer ${this.token}`,
+          cookie: this.cookie,
         },
       })
     ).json();
@@ -51,14 +65,31 @@ class Client {
   }
 }
 
-// console.log(await client.api("https://api.slack.com/methods/conversations.list", {}));
+///////////////////////////////////////////////////////////////////////////////
 
-const cwd = Deno.cwd();
-const writeText = async (path, text) => {
-  await Deno.writeTextFile(path, text);
-};
+// Firefox > Web Developer Tools > Network > Filter URLs: "client.boot"
+// > Save All As HAR: "client.boot.har"
+import boot from "./client.boot.har.json" assert { type: "json" };
 
-const client = new Client(token);
+const team_name = JSON.parse(boot.log.entries[0].response.content.text).team
+  .domain;
+const team_id = JSON.parse(boot.log.entries[0].response.content.text).team.id;
+const cookie = boot.log.entries[0].request.headers.find(
+  (v) => v.name == "Cookie"
+)?.value;
+const token = boot.log.entries[0].request.postData.text
+  .match(/xoxc-.+/)?.[0]
+  .trim();
+
+const client = new Client({ token, cookie, team_id });
+
+/*
+console.log(
+  await client.api("https://api.slack.com/methods/conversations.list", {
+    types: "public_channel,private_channel,mpim,im",
+  })
+);
+*/
 
 const threads = (await stdout("grep", "-hEro", '"thread_ts":"[^"]*"'))
   .split("\n")
